1f5d5a8ba1e067f4b6b270675bd12846b866e24e
Ivan Roschin <ivan.roschin86@gmail.com>
2022-11-02 21:23:22 +0200

    update modal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,63 +1,13 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 import { Box } from '../Box/Box';
-import { PageTitle, ContactsTitle } from './App.stylized';
-import { ContactForm } from '../ContactForm/ContactForm';
-import { Filter } from '../Filter/Filter';
-import { ContactList } from '../ContactList/ContactList';
+import Modal from '../Modal/index';
 
 export class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+    showModal: false,
   };
-
-  addContact = ({ name, number }) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    const isNameExist = this.state.contacts.some(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
-    );
-    if (isNameExist) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
-    console.log('Контакт добавлен');
-  };
-
-  findContact = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  renderContacts = () => {
-    const { contacts, filter } = this.state;
-    let filtered = contacts;
-    if (filter.toLowerCase()) {
-      filtered = contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter)
-      );
-    }
-    return filtered;
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-    console.log('Контакт удален');
-  };
-
   componentDidMount() {
     console.log('App componentDidMount');
     const contacts = localStorage.getItem('contacts');
@@ -75,47 +25,35 @@ export class App extends Component {
     }
   }
 
-  render() {
-    return (
-      <Box
-        pt={5}
-        pl={7}
-        display="flex"
-        flexDirection="column"
-        alignItems="flex-start"
-        justifyContent="space-around"
-        style={{
-          gap: '16px',
-        }}
-      >
-        <PageTitle>Phonebook</PageTitle>
-        <Box
-          display="inline-flex"
-          alignItems="flex-start"
-          justifyContent="space-around"
-          style={{
-            gap: '200px',
-          }}
-        >
-          <Box
-            display="inline-flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <ContactsTitle>Add Contact</ContactsTitle>
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
 
-            <ContactForm onSubmit={this.addContact} />
-          </Box>
-          <Box>
-            <ContactsTitle>Contacts List</ContactsTitle>
-            <Filter findContact={this.findContact} />
-            <ContactList
-              contacts={this.renderContacts()}
-              deleteContact={this.deleteContact}
-            />
-          </Box>
-        </Box>
+  render() {
+    const { showModal } = this.state;
+    return (
+      <Box pt={5} pl={7}>
+        <button type="button" onClick={this.toggleModal}>
+          Open
+        </button>
+
+        {showModal && (
+          <Modal onEsc={this.toggleModal}>
+            {' '}
+            <h1>Привет это контент модалки как children</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+              omnis assumenda hic, nostrum obcaecati aut aperiam illo,
+              laudantium, quasi cumque magni rerum iure odio quibusdam
+              perferendis placeat repellendus. Voluptate, quam!
+            </p>
+            <button type="button" onClick={this.toggleModal}>
+              Close
+            </button>
+          </Modal>
+        )}
       </Box>
     );
   }
