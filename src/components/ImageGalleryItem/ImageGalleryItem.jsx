57fdda475c282d2b342118f4e5efcf9f45c10b8c
Ivan Roschin <ivan.roschin86@gmail.com>
2022-11-05 18:39:39 +0200

    popa s fetch

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,15 +1,15 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-export const ImageGalleryItem = ({ images }) => {
-  return (
-    <li className="gallery-item" key={id}>
-      <img src="" alt="" />
-    </li>
-  );
-};
+// export const ImageGalleryItem = ({ images }) => {
+//   return (
+//     <li className="gallery-item" key={id}>
+//       <img src="" alt="" />
+//     </li>
+//   );
+// };
 
-ImageGalleryItem.propType = {
-  id: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-};
+// ImageGalleryItem.propType = {
+//   id: PropTypes.string.isRequired,
+//   url: PropTypes.string.isRequired,
+//   alt: PropTypes.string.isRequired,
+// };
